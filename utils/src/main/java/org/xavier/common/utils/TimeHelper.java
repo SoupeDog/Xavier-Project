39321c38727dbcd5624b4b4ff09c91cfa31a17d2
Xavier <xavierpe@qq.com>
2018-04-03 00:27:04 +0800

    待完工

---
+++ utils/src/main/java/org/xavier/common/utils/TimeHelper.java
@@ -0,0 +1,35 @@
+package org.xavier.common.utils;
+
+import java.time.LocalDateTime;
+import java.time.ZonedDateTime;
+import java.util.Date;
+
+/**
+ * 描述信息：<br/>
+ * 时间操作工具类 接口
+ *
+ * @author Xavier
+ * @version 1.0
+ * @date 2018/4/2
+ * @since Jdk 1.8
+ */
+public interface TimeHelper {
+
+    /**
+     * 获取当前时间距离 x 天后的 00:00:00 000 剩余毫秒数
+     *
+     * @param x 经过多少个自然日
+     * @return 当前时间距离 x 天后的 00:00:00 剩余毫秒数
+     */
+    Long getDeadlineRemain(Integer x);
+
+    /**
+     * 获取当前瞬时距离 "审判日" 的剩余毫秒数
+     *
+     * @param dommsday
+     * @return
+     */
+    Integer getDeadlineRemain(ZonedDateTime dommsday);
+
+
+}
---
+++ utils/src/main/java/org/xavier/common/utils/base/BaseTimeHelper.java
@@ -0,0 +1,64 @@
+package org.xavier.common.utils.base;
+
+import org.xavier.common.exception.base.ServiceException_Runtime;
+import org.xavier.common.utils.TimeHelper;
+
+import java.time.*;
+
+import static java.time.temporal.ChronoField.*;
+
+/**
+ * 描述信息：<br/>
+ * 基于 TimeHelper 的 时间工具主基类
+ *
+ * @author Xavier
+ * @version 1.0
+ * @date 2018/4/2
+ * @since Jdk 1.8
+ */
+public class BaseTimeHelper implements TimeHelper {
+    /**
+     * 默认该工具环境为 东八区
+     */
+    private static ZoneId CHINA = ZoneId.of("Asia/Shanghai");
+    /**
+     * 一分钟有多少秒
+     */
+    public static final Integer MINUTE_SECOND = 60;
+    /**
+     * 一天有多少秒
+     */
+    public static final Integer DAY_SECOND = 86400;
+
+    public static void main(String[] args) {
+        BaseTimeHelper baseTimeHelper = new BaseTimeHelper();
+        ZonedDateTime zonedDateTime = ZonedDateTime.of(2018, 2, 21, 0, 0, 0, 0, CHINA);
+        System.out.println(baseTimeHelper.getDeadlineRemain(1));
+    }
+
+    @Override
+    public Long getDeadlineRemain(Integer x) {
+        ZonedDateTime currentDateTime = ZonedDateTime.now(CHINA);
+        ZonedDateTime aimDateTime = ZonedDateTime.of(currentDateTime.getYear(), currentDateTime.getMonth().getValue(), currentDateTime.getDayOfMonth() + x, 0, 0, 0, 0, CHINA);
+        if (!aimDateTime.isAfter(currentDateTime)) {
+            throw new ServiceException_Runtime("已过期");
+        }
+        return aimDateTime.toInstant().toEpochMilli() - currentDateTime.toInstant().toEpochMilli();
+    }
+
+    @Override
+    public Integer getDeadlineRemain(ZonedDateTime dommsday) {
+        LocalDateTime currentDateTime = LocalDateTime.now();
+        LocalDateTime dommsdayDateTime = LocalDateTime.ofInstant(dommsday.toInstant(), ZoneId.systemDefault());
+        Period period = Period.between(currentDateTime.toLocalDate(), dommsdayDateTime.toLocalDate());
+        return period.getYears();
+    }
+
+    public static ZoneId getZoneId() {
+        return CHINA;
+    }
+
+    public static void setZoneId(ZoneId zoneId) {
+        CHINA = zoneId;
+    }
+}
