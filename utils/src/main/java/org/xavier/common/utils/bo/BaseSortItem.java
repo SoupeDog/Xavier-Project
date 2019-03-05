483b6383f61059c6cc3fda7805f47d77c9634216
Xavier <xavierpe@qq.com>
2019-03-05 16:08:11 +0800

    描述校对

--- utils/src/main/java/org/xavier/common/utils/bo/BaseSortItem.java
+++ utils/src/main/java/org/xavier/common/utils/bo/BaseSortItem.java
@@ -9,12 +9,13 @@ package org.xavier.common.utils.bo;
  * @date 2018.11.30
  * @since Jdk 1.8
  */
-public interface BaseSortItem {
+public interface BaseSortItem<T> {
     /**
-     * 用自身去和另一个同类对象比较，返回当前元素的相对大小描述
+     * 用自身去和另一个同类对象比较，返回第一个元素的相对大小描述信息
      *
-     * @param another 参照物
-     * @return 自身与参照物的关系
+     * @param currentItem 当前元素
+     * @param anotherItem 参照物
+     * @return 当前元素与参照物的相对大小关系
      */
-    CompareRelativeResultEnum toCompareAnother(BaseSortItem another);
+    CompareRelativeResultEnum toCompareAnother(T currentItem, T anotherItem);
 }
